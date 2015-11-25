0e2c1ca9e57bb60ae035364aa212f93b3e3c348b
deftnesssolutions <gusove@gmail.com>
2015-11-25 10:01:03 -0200

    commit: criação das clases entidadades

---
+++ Axado/Entities/CarriersEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities
+{
+    public class CarriersEntity
+    {
+        public int id_carrier { get; set; }
+        public string tipo { get; set; }
+        public string nome { get; set; }
+        public string endereco { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+        public string cpf { get; set; }
+        public string cnpj { get; set; }
+        public string inscr_estadual { get; set; }
+        public string nro_placa { get; set; }
+        public string estado_placa { get; set; }
+        public int id_classificacao { get; set; }
+    }
+}
---
+++ Axado/Entities/ClassificacaoEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entities
+{
+    public class ClassificacaoEntity
+    {
+        public int id_classificacao { get; set; }
+        public string tipo { get; set; }
+        public string descricao { get; set; }
+    }
+}
