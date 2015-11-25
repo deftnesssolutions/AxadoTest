7051a6bc04bf31713935bd7e7990dd1444dc7083
deftnesssolutions <gusove@gmail.com>
2015-11-25 09:13:13 -0200

    commit criação das interfaces de gestão de data base

---
+++ Axado/DataAccess/Connection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    /*######
+     * Programa:Implementación de la Interface de conexión al Base de Datos
+     * Autor: GUSTAVO OVELAR
+     * Fecha: 04/11/2012
+    #####*/
+
+    //Aqui creamos la conexión propiamente dicha, asignando el String de Conexión, la apertura de la Base de Datos
+    //el Cierre y la Liberación de memoria atravez del Garbage Collector(GC.SuppressFinalize(this))
+    public class Connection : IConnection
+    {
+        private SqlConnection _connectionString;
+
+        #region Constructor de la Clase
+        //Constructor
+        public Connection()
+        {
+            _connectionString = new SqlConnection("User Id=postgres;Password=postgres;Host=localhost;Database=axadotest");
+        }
+        #endregion
+
+        #region Implementación de Metodos de la Interface
+
+        //Abre conexión
+        public SqlConnection Open()
+        {
+            if (_connectionString.State == ConnectionState.Closed)
+            {
+                _connectionString.Open();
+            }
+            return _connectionString;
+        }
+
+        //Retorna conexión
+        public SqlConnection Get()
+        {
+            return this.Open();
+        }
+
+        //Cierra conexión
+        public void Close()
+        {
+            if (_connectionString.State == ConnectionState.Open)
+            {
+                _connectionString.Close();
+            }
+        }
+        //Llama el Garbage Collector
+        public void Dispose()
+        {
+            this.Close();
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ Axado/DataAccess/IDao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess
+{
+    /*######
+    * Programa:Interface DAO PARA CRUD de entidades en Banco de Datos
+    * Autor: GUSTAVO OVELAR
+    * Fecha: 04/11/2012
+   #####*/
+
+    //Interface que sirve de base para la manipulación de datos de las Tablas en el banco de datos con utilización del colector de basura(Garbage Collector)
+    //que sera el encargado de limpiar qualquier objeto sin uso que este utilizando  memoria
+    public interface IDao<T> : IDisposable
+        where T : class,new()
+    {
+        T Insert(T model);//metodo para insertar registro
+        bool Update(T model);//metodo para actualizar registro
+        bool Delete(T model);//metodo para eliminar registro 
+        T FindOrDefault(params object[] keys);//metodo para retornar un registro
+        IEnumerable<T> All();//metodo para retornar todos los registros
+        IEnumerable<T> rowsFilter(string p1, string p2, string p3);//metodo para retornar registros filtrado por parametros
+    }
+}
